f878d67b63bc0b49dca715692db82789611458b8
Ivan A Rodriguez <ivancalvo91@gmail.com>
2024-09-15 04:31:51 -0400

    setup docker for client app

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -16,12 +19,27 @@ type User struct {
 }
 
 func main() {
-	fmt.Println("Hello World")
+
+	godotenv.Load(".env")
+
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	app.Use(csrf.New())
 
 	app.Get("/test", func(ctx *fiber.Ctx) error {
 		return ctx.Status(200).JSON(fiber.Map{"message": "test endpoint is working"})
 	})
 
-	log.Fatal(app.Listen(":5000"))
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Could not get PORT environment variable")
+	}
+
+	log.Fatal(app.Listen(":" + portString))
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		watch: {
+			usePolling: true,
+		},
+	},
+	// We must determine in which port the app will run when
+	// we execute pnpm run preview
+	// host true will expose the project in public addresses
+	preview: {
+		host: true,
+		port: 8080,
+	},
 });
